d7bf3c8bae88de010cdf07ca24ec9f23496025bc
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-09-29 19:06:42 -0300

    feat: Implementação da relação um para muitos

--- src/Produto/entities/produto.entity.ts
+++ src/Produto/entities/produto.entity.ts
@@ -1,5 +1,6 @@
 import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Categoria } from "../../Categoria/entities/categoria.entity";
 
 @Entity({name: "tb_produtos"})
 export class Produto{
@@ -24,4 +25,9 @@ export class Produto{
 
     @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
     preco: number;
+
+    @ManyToOne(() => Categoria, (categoria) => categoria.produto, {
+        onDelete: "CASCADE"
+    })
+    categoria: Categoria
 }
