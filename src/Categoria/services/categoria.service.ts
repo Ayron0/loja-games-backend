2b5a3310c9a8f6b83707899d9c0ae8696246bdf2
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-09-29 19:06:02 -0300

    feat: Implementação da classe Categoria Service

---
+++ src/Categoria/services/categoria.service.ts
@@ -0,0 +1,68 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { Categoria } from "../entities/categoria.entity";
+
+@Injectable()
+export class CategoriaService {
+    constructor(
+        @InjectRepository(Categoria)
+        private CategoriaRepository: Repository<Categoria>
+    ) { }
+
+    async findAll(): Promise<Categoria[]> {
+        return await this.categoriaRepository.find({
+            relations: {
+                produto: true 
+            }
+        });
+    }
+
+    async findById(id: number): Promise<Categoria> {
+
+        let Categoria = await this.categoriaRepository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                produto: true
+            }
+        });
+
+        if (!Categoria)
+            throw new HttpException('Categoria não encontrado!', HttpStatus.NOT_FOUND);
+
+        return Categoria;
+    }
+
+    async findAllByTipo(tipo: string): Promise<Categoria[]> {
+        return await this.categoriaRepository.find({
+            where: {
+                tipo: ILike(`%${tipo}%`)
+            },
+            relations: {
+                produto: true
+            }
+        })
+    }
+
+    async create(Categoria: Categoria): Promise<Categoria> {
+        return await this.CategoriaRepository.save(Categoria);
+    }
+
+    async update(categoria: Categoria): Promise<Categoria> {
+
+        await this.findById(Categoria.id);
+
+        return await this.categoriaRepository.save(categoria);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        await this.findById(id);
+
+        return await this.categoriaRepository.delete(id);
+
+    }
+
+}
